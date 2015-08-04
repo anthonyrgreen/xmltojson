4c41042268dd3206b844cb8cd6b3373a4ca82e6b
anthonyrgreen <argreen@umich.edu>
2015-08-04 16:11:52 -0400

    added argparse interface

--- main.py
+++ main.py
@@ -1,87 +1,12 @@
-import lxml.etree as ET
-import json as JS
-import uuid
+from xmlToJson import treeParser, toJsonSchema
+from argparse import ArgumentParser
 
-class treeParser:
-  def __init__(self, filename, parserFunction):
-    xmlFile = open(filename, "rb")
-    self.parserFunction = parserFunction
-    tree = ET.iterparse(filename, events=("start", "end"))
-    self.tree = iter(tree)
-    _, self.root = self.tree.next()
-  def __iter__(self):
-    return self.iterator()
-  def iterator(self):
-    for event, elem in self.tree:
-      if event == "end" and elem.tag == "ClinVarSet":
-        yield self.parserFunction(elem)
-        self.root.clear()
+parser = ArgumentParser(description="Transform XML records to JSON records")
+parser.add_argument("filename", help="The XML input file.")
 
-def toJson(element):
-
-  def singleAttrFromPath(path, attrType, attrName="", modFunc = lambda x: x):
-    # in the case of an attribute
-    if attrType == "attr":
-      node = element.find(path)
-      if node == None:
-        return "Undefined"
-      else:
-        return modFunc(node.get(attrName, "Undefined"))
-    # get the text
-    else:
-      return modFunc(element.findtext(path, default="Undefined"))
-
-  def allAttrsFromPath(path, attrType, attrName="",
-                       predicateFunc=lambda _: True, modFunc=lambda x: x):
-    nodes = [node for node in element.findall(path) if predicateFunc(node)]
-    # in the case of an attribute
-    if attrType == "attr":
-      attrs = [node.get(attrName, "Undefined") for node in nodes if predicateFunc(node)]
-      return map(modFunc, attrs)
-    # in the case of text
-    else:
-      return [modFunc(node.text) for node in nodes]
-
-  # modification functions
-  noMod       = lambda x: x.encode("utf-8")
-  rsIdMod     = lambda x: "rs" + x.encode("utf-8")
-  # predicate functions
-  noPredicate = lambda _: True
-  hvgsPred    = lambda x: "HVGS" in x.get("DB", default="Undefined")
-  rsIdPred    = lambda x: x.get("DB", default="Undefined") == "dbSNP"
-  entrezPred  = lambda x: x.get("DB", default="Undefined") == "Gene"
-  # path constants
-  base        = "ReferenceClinVarAssertion/"
-  measureBase = base + "MeasureSet/Measure/"
-  # FIELDS
-  # (name, path, text/attr, attr_name, type_func, mod_func)
-  singleProperties = \
-    [("rcvaccession",          base + "ClinVarAccession",                 "attr", "Acc",     str, noMod)
-    ,("rcvaccession_version",  base + "ClinVarAccession",                 "attr", "Version", int, noMod)
-    ,("type",                  base + "MeasureSet/Measure",               "attr", "Type",    str, noMod)
-    ,("clinical_significance", base + "ClinicalSignificance/Description", "text", "",        str, noMod)
-    ,("title",                 "Title",                                   "text", "",        str, noMod)
-    ,("preferred_name",        measureBase + "Name/ElementValue",         "text", "",        str, noMod)]
-  # (name, path, text/attr, attr_name, type_func, mod_func, pred_func)
-  multipleProperties = \
-    [("hgvs",           measureBase + "AttributeSet/Attribute",   "text", "",   str, noMod,   hvgsPred)
-    ,("entrez_gene_id", measureBase + "MeasureRelationship/XRef", "attr", "ID", str, noMod,   entrezPred)
-    ,("rs_id",          measureBase + "XRef",                     "attr", "ID", str, rsIdMod, rsIdPred)]
-
-  result = {}
-  for prop in singleProperties:
-    result[prop[0]] = prop[4](singleAttrFromPath(prop[1], prop[2],
-                              attrName=prop[3], modFunc=prop[5]))
-  for prop in multipleProperties:
-    result[prop[0]] = map(prop[4], allAttrsFromPath(prop[1], prop[2],
-                              attrName=prop[3], modFunc=prop[5], predicateFunc=prop[6]))
-  result["rcvaccession_full"] = result["rcvaccession"] + "." + str(result["rcvaccession_version"])
-  result["uuid"] = uuid.uuid4().hex
-
-  return JS.JSONEncoder().encode(result)
-
-tree = treeParser("ClinVarFullRelease_2014-08.xml", toJson)
-f = open("jsonOutput", "w")
-for elem in tree:
-  f.write(elem + "\n")
-f.close()
+if __name__ == "__main__":
+  args = parser.parse_args()
+  tree = treeParser(args.filename, toJsonSchema)
+  with open(args.filename + ".json", "w") as output:
+    for elem in tree:
+      output.write(elem + "\n")
---
+++ xmlToJson.py
@@ -0,0 +1,86 @@
+import lxml.etree as ET
+import json as JS
+import uuid
+
+class treeParser:
+  def __init__(self, filename, parserFunction):
+    xmlFile = open(filename, "rb")
+    self.parserFunction = parserFunction
+    tree = ET.iterparse(filename, events=("start", "end"))
+    self.tree = iter(tree)
+    _, self.root = self.tree.next()
+  def __iter__(self):
+    return self.iterator()
+  def iterator(self):
+    for event, elem in self.tree:
+      if event == "end" and elem.tag == "ClinVarSet":
+        yield self.parserFunction(elem)
+        self.root.clear()
+
+def toJsonSchema(element):
+
+  noMod       = lambda x: x.encode("utf-8")
+  noPredicate = lambda _: True
+
+  def getAttr(path, attrType, attrName="", modFunc=noMod):
+    # in the case of an attribute
+    if attrType == "attr":
+      node = element.find(path)
+      if node == None:
+        return "Undefined"
+      else:
+        return modFunc(node.get(attrName, "Undefined"))
+    # get the text
+    else:
+      return modFunc(element.findtext(path, default="Undefined"))
+
+  def getAttrs(path, attrType, attrName="", predicateFunc=noPredicate, modFunc=noMod):
+    nodes = [node for node in element.findall(path) if predicateFunc(node)]
+    # in the case of an attribute
+    if attrType == "attr":
+      return [modFunc(node.get(attrName, "Undefined"))
+              for node in nodes if predicateFunc(node)]
+    # in the case of text
+    else:
+      return [modFunc(node.text) for node in nodes]
+
+  result = {}
+  # path constants
+  base        = "ReferenceClinVarAssertion/"
+  measureBase = base + "MeasureSet/Measure/"
+  # single-valued attributes
+  result["rcvaccession_version"]  = int(getAttr(base + "ClinVarAccession",
+                                                "attr", "Version"))
+  result["rcvaccession"]          = getAttr(base + "ClinVarAccession",
+                                            "attr", "Acc")
+  result["type"]                  = getAttr(base + "MeasureSet/Measure",
+                                            "attr", "Type")
+  result["clinical_significance"] = getAttr(base + "ClinicalSignificance/Description",
+                                            "text")
+  result["title"]                 = getAttr("Title",
+                                            "text")
+  result["preferred_name"]        = getAttr(measureBase + "Name/ElementValue",
+                                            "text")
+  # multiple-valued attributes
+  # predicate and modification functions
+  hvgsPred    = lambda x: "HVGS" in x.get("DB", default="Undefined")
+  rsIdPred    = lambda x: x.get("DB", default="Undefined") == "dbSNP"
+  entrezPred  = lambda x: x.get("DB", default="Undefined") == "Gene"
+  rsIdMod     = lambda x: "rs" + x.encode("utf-8")
+
+  result["hgvs"]           = getAttrs(measureBase + "AttributeSet/Attribute",
+                                      "text",
+                                      predicateFunc=hvgsPred)
+  result["entrez_gene_id"] = getAttrs(measureBase + "MeasureRelationship/XRef",
+                                      "attr", "ID",
+                                      predicateFunc=entrezPred)
+  result["rs_id"]          = getAttrs(measureBase + "XRef",
+                                      "attr", "ID",
+                                      modFunc=rsIdMod,
+                                      predicateFunc=rsIdPred)
+  # extra attributes
+  result["rcvaccession_full"] = result["rcvaccession"] + "." + \
+                                str(result["rcvaccession_version"])
+  result["uuid"] = uuid.uuid4().hex
+
+  return JS.JSONEncoder().encode(result)
